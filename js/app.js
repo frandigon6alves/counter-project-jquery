299fe4a6ff150cbe6c4dbdf8f7a92878bf256fb8
Franciellen <franciellen_gc@hotmail.com>
2023-05-09 12:17:51 +0200

    listo

--- js/app.js
+++ js/app.js
@@ -1,6 +1,33 @@
-let counter = 0;
+var counter = 0;
 
 $(document).ready(function () {
     console.log("DOM cargado! Implementa tu solución a partir de esta línea.");
 
+
+    $('.prevBtn').click(function () {
+        counter--;
+        updateDOM();
+    });
+
+    $('.nextBtn').click(function () {
+        counter++;
+        updateDOM();
+    });
+
+    function updateDOM() {
+        $('#counter').text(counter);
+
+        if (counter == 0) {
+            $('#counter').removeClass('positivo');
+            $('#counter').removeClass('negativo');
+        }
+
+        else if (counter > 0) {
+            $('#counter').addClass('positivo');
+        }
+
+        else {
+            $('#counter').addClass('negativo');
+        }
+    }
 });
